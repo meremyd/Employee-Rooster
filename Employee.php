cf98fcd22ed394e84754dd8f6ec0de868c4e7b42
meremyd <128643046+meremyd@users.noreply.github.com>
2024-11-07 16:31:05 +0800

    Update Employee.php

--- Employee.php
+++ Employee.php
@@ -7,5 +7,17 @@ abstract class Employee extends Person {
         $this->companyName = $companyName;
     }
 
+    public function getCompanyName(){
+        return $this->companyName;
+    }
+
+    public function setCompanyName($companyName){
+        $this->companyName = $companyName;
+    }
+
     abstract public function earnings();
+
+    public function __toString(){
+        return $this->companyName;
+    }
 }
