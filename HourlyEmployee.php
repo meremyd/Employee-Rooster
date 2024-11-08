d7a9e3e6b27b9a1df21343b5a2fab09d3b315c68
meremyd <128643046+meremyd@users.noreply.github.com>
2024-11-08 12:59:41 +0800

    Update HourlyEmployee.php

--- HourlyEmployee.php
+++ HourlyEmployee.php
@@ -1,11 +1,23 @@
 <?php
+require_once 'Employee.php';
 
 class HourlyEmployee extends Employee {
-    private $hoursWorked;
-    private $hourlyRate;
+    private int $hoursWorked;
+    private float $rate;
 
-    public function __construct($hoursWorked, $hourlyRate) {
+    public function __construct($name, $address, $age, $companyName, $hoursWorked, $rate) {
+        parent::__construct($name, $address, $age, $companyName);
         $this->hoursWorked = $hoursWorked;
-        $this->hourlyRate = $hourlyRate;
+        $this->rate = $rate;
+    }
+
+    public function earnings(): float {
+        $overtimeRate = ($this->hoursWorked > 40) ? 1.5 * $this->rate : $this->rate;
+        return $this->hoursWorked * $overtimeRate;
+    }
+
+    public function __toString(): string {
+        return parent::__toString() . ", Hours Worked: {$this->hoursWorked}, Rate: {$this->rate}";
     }
 }
+ 
