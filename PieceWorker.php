23c0222e46610422fab2d60959f4a43df104c47c
meremyd <128643046+meremyd@users.noreply.github.com>
2024-11-08 13:00:59 +0800

    Update PieceWorker.php

--- PieceWorker.php
+++ PieceWorker.php
@@ -1,11 +1,22 @@
 <?php
+require_once 'Employee.php';
 
 class PieceWorker extends Employee {
-    private $itemsProduced;
-    private $wagePerItem;
+    private int $itemsProduced;
+    private float $wagePerItem;
 
-    public function __construct($itemsProduced, $wagePerItem) {
+    public function __construct($name, $address, $age, $companyName, $itemsProduced, $wagePerItem) {
+        parent::__construct($name, $address, $age, $companyName);
         $this->itemsProduced = $itemsProduced;
         $this->wagePerItem = $wagePerItem;
     }
+
+    public function earnings(): float {
+        return $this->itemsProduced * $this->wagePerItem;
+    }
+
+    public function __toString(): string {
+        return parent::__toString() . ", Items Produced: {$this->itemsProduced}, Wage per Item: {$this->wagePerItem}";
+    }
 }
+ 
