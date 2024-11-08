674716f9beb4a864d55574cf0fb774072dba3c4b
meremyd <128643046+meremyd@users.noreply.github.com>
2024-11-08 13:01:48 +0800

    Update CommissionEmployee.php

--- CommissionEmployee.php
+++ CommissionEmployee.php
@@ -1,13 +1,24 @@
 <?php
+require_once 'Employee.php';
 
 class CommissionEmployee extends Employee {
-    private $regularSalary;
-    private $itemSold;
-    private $commissionRate;
+    private float $regularSalary;
+    private int $itemsSold;
+    private float $commissionRate;
 
-    public function __construct($regularSalary,  $itemSold, $commissionRate) {
-        $this->regularySalary = $regularSalary;
-        $this->itemSold = $itemSold;
+    public function __construct($name, $address, $age, $companyName, $regularSalary, $itemsSold, $commissionRate) {
+        parent::__construct($name, $address, $age, $companyName);
+        $this->regularSalary = $regularSalary;
+        $this->itemsSold = $itemsSold;
         $this->commissionRate = $commissionRate;
     }
-}
+
+    public function earnings(): float {
+        return $this->regularSalary + ($this->itemsSold * $this->commissionRate);
+    }
+
+    public function __toString(): string {
+        return parent::__toString() . ", Salary: {$this->regularSalary}, Items Sold: {$this->itemsSold}, Commission Rate: {$this->commissionRate}";
+    }
+} 
+
