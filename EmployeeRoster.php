baf8972e132de17ad11493693cba96c0248cb774
meremyd <128643046+meremyd@users.noreply.github.com>
2024-11-08 12:59:16 +0800

    Update EmployeeRoster.php

--- EmployeeRoster.php
+++ EmployeeRoster.php
@@ -1,37 +1,56 @@
 <?php
 
-class EmployeeRoser {
-    private $roster = [];
+class EmployeeRoster {
+    private array $roster;
 
     public function __construct($rosterSize) {
-        $this->roster = array_fill(0, $rosterSize, null);
+        $this->roster = array();
+        echo "Roster initialized with capacity: $rosterSize\n";
     }
 
     public function add(Employee $employee) {
-
+        
+        if ($this->hasEmployeeWithName($employee->getName())) {
+            echo "An employee with the name '{$employee->getName()}' already exists.\n";
+        } else {
+            $this->roster[] = $employee;
+            echo "Employee added successfully.\n";
+        }
     }
 
-    public function remove($employee_id) {
-
+    public function hasEmployeeWithName($name): bool {
+        foreach ($this->roster as $employee) {
+            if ($employee->getName() === $name) {
+                return true; 
+            }
+        }
+        return false;  
     }
 
-    public function count() {
-
+    public function remove($index) {
+        if (isset($this->roster[$index])) {
+            unset($this->roster[$index]);
+            $this->roster = array_values($this->roster); 
+            echo "Employee removed.\n";
+        } else {
+            echo "Invalid index.\n";
+        }
     }
 
-    public function countCE() {
-
+    public function count(): int {
+        return count($this->roster);
     }
 
-    public function countHE() {
-
-    }
-    
-    public function countPE() {
-
+    public function display() {
+        foreach ($this->roster as $employee) {
+            echo $employee . "\n";
+        }
     }
 
     public function payroll() {
-
+        foreach ($this->roster as $employee) {
+            echo $employee->getName() . " - Earnings: $" . $employee->earnings() . "\n";
+        }
     }
-}
+} 
+
